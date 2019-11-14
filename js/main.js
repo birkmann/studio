758d2de978ba5afa2b19aad766d641f969aa1c61
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-11-14 11:01:16 +0100

    files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$("body").removeClass("preload");
+});
+
+$(window).scroll(function(){
+
+	if ( $(this).scrollTop() > 200 ) { 
+		$('body').addClass("scrolled");
+	} else { 
+		$('body').removeClass("scrolled");
+	}
+
+});
+
+
+$('a[href^="#"]').on('click',function(e) {
+	e.preventDefault();
+	var offset = 0;
+	var target = this.hash;
+	if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+	$('html, body').stop().animate({
+		'scrollTop': $(target).offset().top - 125
+	}, 500, 'swing', function() {
+		// window.location.hash = target;
+	});
+});
